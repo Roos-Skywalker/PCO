29775b267dde20f08421decb88c4e7fdcff089db
Roos-Skywalker <grifioen@live.nl>
2025-09-23 13:33:49 +0200

    Improved variable names

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -2,14 +2,14 @@
  soundeffect element in HTML points to the location the use must click on.
  */
 function addEvent() {
-  const a = document.getElementById("soundeffect");
-  a.addEventListener("click", play);
+  const findClickLocation = document.getElementById("soundeffect");
+  findClickLocation.addEventListener("click", play);
 }
 
 /**
  The audio element in HTML contains a reference to audio file, which is then played by line 14.
  */
 function play() {
-  const audio = document.getElementById("audio");
-  audio.play();
+  const audioFileInHTML = document.getElementById("audio");
+  audioFileInHTML.play();
 }
