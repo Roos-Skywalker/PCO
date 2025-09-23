fd1aa04876ee13e622495282a6a494a72fa05b7c
Roos-Skywalker <grifioen@live.nl>
2025-09-23 13:26:09 +0200

    Thank you ESlint

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,8 +1,15 @@
+/**
+ soundeffect element in HTML points to the location the use must click on.
+ */
 function addEvent() {
-    var a = document.getElementById("soundeffect");
-    a.addEventListener("click", play);
+  const a = document.getElementById("soundeffect");
+  a.addEventListener("click", play);
 }
+
+/**
+ The audio element in HTML contains a reference to audio file, which is then played by line 14.
+ */
 function play() {
-    var audio = document.getElementById("audio");
-    audio.play();
+  const audio = document.getElementById("audio");
+  audio.play();
 }
