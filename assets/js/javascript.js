7643e1bf5c214ba26c33080d16977ddbea2ca581
Roos-Skywalker <grifioen@live.nl>
2025-10-25 14:45:16 +0200

    Javascript now dynamically checks for a passing grade dynamically and plays the correct sound

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,15 +1,43 @@
+window.addEventListener('load', onWindowLoad);
+
 /**
- soundeffect element in HTML points to the location the use must click on.
+ * 
  */
-function addEvent() {
-  const findClickLocation = document.getElementById("soundeffect");
-  findClickLocation.addEventListener("click", play);
+function onWindowLoad() {
+  const gradeClass = document.getElementsByClassName('grade'); //First declare a variable that contains all DOM elements with the classname gradeElement.
+  //This returns a collection, which can be iterated like an array.
+  for (i = 0; i < gradeClass.length; i++) { //Iterate through the collection, but do not use as-is.
+    const gradeElement = gradeClass[i]; //Gain access to the individual class elements. Because the parent element is looped, you gain access to them all. Work from here.
+    const grade = parseFloat(gradeElement.innerText); //Declare the actual graded number inside the HTML and force it to become a number.
+    addEvent(gradeElement, grade); // Pass the following variables to the next function.
+  }
 }
 
 /**
- The audio element in HTML contains a reference to audio file, which is then played by line 14.
+ * 
+ * @param {Document} gradeElement Child from the gradeClass variable.
+ * @param {Number} grade Child's innertext value parsed into a floating point number. Used to pass to next function.
  */
-function play() {
-  const audioFileInHTML = document.getElementById("audio");
-  audioFileInHTML.play();
+function addEvent(gradeElement, grade) {
+  gradeElement.addEventListener("click", function() {
+    playSound(grade);
+  });
+}
+
+/**
+ * Test whether or not the grade passes, then plays the corresponding sound.
+ * @param {Number} grade gradeClass.gradeElement.innerText floating point value representing grade.
+ */
+function playSound(grade) {
+  if (grade >= 5.5) {
+    const passAudioInHTML = document.getElementById("pass");
+    passAudioInHTML.play();
+    console.log(`Passed grade ${grade}`);
+  } else if (grade < 5.5) {
+    const failAudioInHTML = document.getElementById("fail");
+    failAudioInHTML.play();
+    console.log(`Failed grade ${grade}`);
+  } else {
+    console.warn(`This grade is not a number. ${grade}.`)
+  }
 }
