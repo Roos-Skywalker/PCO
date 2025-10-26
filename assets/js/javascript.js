b05ab3674a914e2e837a1e6659dbbd4cfba78213
Roos-Skywalker <grifioen@live.nl>
2025-10-26 10:44:13 +0100

    Improved naming

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -4,19 +4,19 @@ window.addEventListener('load', onWindowLoad);
  * 
  */
 function onWindowLoad() {
-  const gradeClass = document.getElementsByClassName('grade'); //First declare a variable that contains all DOM elements with the classname gradeElement.
+  const gradeClasses = document.getElementsByClassName('grade'); //First declare a variable that contains all DOM elements with the classname gradeElement.
   //This returns a collection, which can be iterated like an array.
-  for (i = 0; i < gradeClass.length; i++) { //Iterate through the collection, but do not use as-is.
-    const gradeElement = gradeClass[i]; //Gain access to the individual class elements. Because the parent element is looped, you gain access to them all. Work from here.
+  for (i = 0; i < gradeClasses.length; i++) { //Iterate through the collection, but do not use as-is.
+    const gradeElement = gradeClasses[i]; //Gain access to the individual class elements. Because the parent element is looped, you gain access to them all. Work from here.
     const grade = parseFloat(gradeElement.innerText); //Declare the actual graded number inside the HTML and force it to become a number.
     testForGrade(gradeElement, grade); // Call function to test whether the grade passes. Grade
   }
 }
 
 /**
- * Uses an if-statement to determine whether the grade passes or not, then call the next function.
- * @param {Document} gradeElement Child from the gradeClass variable.
- * @param {Number} grade Child's innertext value parsed into a floating point number. Then passed to the next function as a string parameter.
+ * Uses an if-statement to determine whether the grade passes or not, then call the next function with a pre-determined parameter value based on the check.
+ * @param {Document} gradeElement Child from the gradeClasses variable.
+ * @param {Number} grade Child's innertext value parsed into a floating point number.
  * @returns 
  */
 function testForGrade(gradeElement, grade) {
@@ -35,20 +35,20 @@ function testForGrade(gradeElement, grade) {
 
 /**
  * Adds an event listener with the CSS class as the grade parameter, string type. Based on the testForGrade function is either 'pass' or 'fail'.
- * @param {Document} gradeElement Child from the gradeClass variable.
- * @param {className} grade Child's innertext set as a string, either 'pass' or 'fail'.
+ * @param {Document} gradeElement Child from the gradeClasses variable.
+ * @param {className} gradeClass Child's innertext set as a string, either 'pass' or 'fail'.
  */
-function addEvent(gradeElement, grade) {
+function addEvent(gradeElement, gradeClass) {
   gradeElement.addEventListener("click", function() {
-    playSound(grade);
+    playSound(gradeClass);
   });
 }
 
 /**
  * Test whether or not the grade passes, then plays the corresponding sound.
- * @param {className} grade Child's innertext set as a string, either 'pass' or 'fail'.
+ * @param {className} gradeClass Child's innertext set as a string, either 'pass' or 'fail'.
  */
-function playSound(grade) {
-    const gradeAudioInHTML = document.getElementById(grade);
+function playSound(gradeClass) {
+    const gradeAudioInHTML = document.getElementById(gradeClass);
     gradeAudioInHTML.play();
 }
